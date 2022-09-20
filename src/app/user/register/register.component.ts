3af887c21570360c14595370ce38dddd4d6d6f6b
shiyuyullin <dayu19990519@gmail.com>
2022-09-19 22:05:15 -0400

    Start to implement user auth modal (reactive form)

---
+++ src/app/user/register/register.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css'],
+})
+export class RegisterComponent {
+  name = new FormControl('', [Validators.required, Validators.minLength(3)]);
+  email = new FormControl('');
+  age = new FormControl('');
+  password = new FormControl('');
+  confirmedPassword = new FormControl('');
+  phoneNumber = new FormControl('');
+
+  registerForm = new FormGroup({
+    name: this.name,
+    email: this.email,
+    age: this.age,
+    password: this.password,
+    confirmedPassword: this.confirmedPassword,
+    phoneNumber: this.phoneNumber,
+  });
+}
